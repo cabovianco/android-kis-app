a019e30e88ac7aded0277e96e941956a7dbfdf7d
thomas <github@cabovianco.anonaddy.me>
2026-07-14 20:03:47 -0300

    Add data classes for Secret and User documents in Firestore

---
+++ app/src/main/java/com/cabovianco/kis/data/remote/firebase/firestore/document/Secret.kt
@@ -0,0 +1,6 @@
+package com.cabovianco.kis.data.remote.firebase.firestore.document
+
+data class Secret(
+    val content: String,
+    val from: String
+)
---
+++ app/src/main/java/com/cabovianco/kis/data/remote/firebase/firestore/document/User.kt
@@ -0,0 +1,6 @@
+package com.cabovianco.kis.data.remote.firebase.firestore.document
+
+data class User(
+    val email: String,
+    val username: String
+)
