3ccc5d4032d07ad1d2d6b0258492c1f4d7f6e8ad
thomas <github@cabovianco.anonaddy.me>
2026-07-14 20:04:51 -0300

    Implement send method in FirebaseSecretRepository to send secrets

--- app/src/main/java/com/cabovianco/kis/data/remote/firebase/repository/FirebaseSecretRepository.kt
+++ app/src/main/java/com/cabovianco/kis/data/remote/firebase/repository/FirebaseSecretRepository.kt
@@ -1,19 +1,61 @@
 package com.cabovianco.kis.data.remote.firebase.repository
 
+import android.util.Log
+import com.cabovianco.kis.data.remote.firebase.firestore.Collection
+import com.cabovianco.kis.data.remote.firebase.firestore.document.Secret
 import com.cabovianco.kis.domain.model.SecretComposeItem
 import com.cabovianco.kis.domain.repository.SecretRepository
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class FirebaseSecretRepository @Inject constructor(
+private const val TAG = "FirebaseSecretRepository"
 
+class FirebaseSecretRepository @Inject constructor(
+    private val auth: FirebaseAuth,
+    private val firestore: FirebaseFirestore
 ) : SecretRepository {
     override suspend fun send(secret: SecretComposeItem): Result<Unit> {
-        withContext(Dispatchers.IO) {
-            Thread.sleep(2000)
-        }
+        return try {
+            val to = secret.to
+            val content = secret.content
+            val from = findUsername()
 
-        return Result.success(Unit)
+            val secret = Secret(
+                from = from,
+                content = content
+            )
+
+            firestore.collection(Collection.INBOX.path)
+                .document(to)
+                .collection(Collection.RECEIVED.path)
+                .add(secret)
+                .addOnSuccessListener {
+                    Log.i(TAG, "Secret sent successfully")
+                }
+                .addOnFailureListener {
+                    Log.e(TAG, "Failed to send secret")
+                }
+                .await()
+
+            Result.success(Unit)
+
+        } catch (ex: Exception) {
+            Result.failure(ex)
+        }
+    }
+
+    private suspend fun findUsername(): String {
+        val uid = auth.currentUser?.uid ?: throw Exception("User not found")
+
+        val snapshot = firestore.collection(Collection.USER.path)
+            .document(uid)
+            .get()
+            .await()
+
+        return snapshot.getString("username") ?: throw Exception("Username not found")
     }
 }
